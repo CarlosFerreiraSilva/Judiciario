ebbe9e58010dc3173546d8565a4cb8642e2efe13
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2022-11-18 15:55:12 -0300

    Update Program.cs

--- JuridicoSolutions/Juridico/Program.cs
+++ JuridicoSolutions/Juridico/Program.cs
@@ -6,24 +6,7 @@ namespace Juridico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Quiz de Rpg");
-            Console.WriteLine("Qual dessas armas você pega? ");
-            Console.WriteLine("[1]Espada ");
-            Console.WriteLine("[2]Cajado ");
-            Console.WriteLine("[3]Livro ");
-            int opt = Convert.ToInt32(Console.ReadLine());
-            if (opt==1)
-            {
-
-            }
-            if (opt==2)
-            {
-
-            }
-            if (opt==3)
-            {
-
-            }
+            Console.WriteLine("Hello Gota Serena!");
         }
     }
 }
